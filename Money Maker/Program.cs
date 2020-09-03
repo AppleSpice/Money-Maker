136681ba51d8948bfe7eff35822e2b94b564c0d8
Gaven Rudiak <gavenrudiak@outlook.com>
2020-09-03 10:52:30 -0400

    Added Code

--- Money Maker/Program.cs
+++ Money Maker/Program.cs
@@ -6,7 +6,33 @@ namespace Money_Maker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MoneyMaker();
+        }
+
+        static void MoneyMaker()
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.WriteLine("Enter an amount of change");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{amount} is equal to about...");
+
+            int goldVal = 10;
+            int silverVal = 5;
+
+            //gold
+            double gold = Math.Floor(amount / goldVal);
+            double remander = amount % goldVal;
+
+            //silver
+            double silver = Math.Floor(remander / silverVal);
+            remander = remander % silverVal;
+
+            //results
+            Console.WriteLine($"{gold} gold coins");
+            Console.WriteLine($"{silver} silver coins");
+            Console.WriteLine($"{remander} bronze coins");
         }
     }
 }
